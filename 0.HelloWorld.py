e57ed38098a237a93b38159858cc6797aff7568e
sharitashya <haritashya@gmail.com>
2019-06-29 12:45:44 +0530

    Space before ! removed

--- 0.HelloWorld.py
+++ 0.HelloWorld.py
@@ -1,7 +1,7 @@
 '''
 Author - Shreeyash Haritashya
 Date - 29th June 2019
-Description - Printing Hello World ! using python programming language
+Description - Printing Hello World! using python programming language
 '''
 
 print("Hello World! \nWelcome to the world of Python 3 programming.")
